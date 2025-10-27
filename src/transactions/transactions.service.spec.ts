c480eadf57c5814cd0017e880b004483737c7487
Oliver Perboni <oliverperboni@gmail.com>
2025-10-27 23:24:52 +0000

    ✨  update Jest configuration and improve module path mapping in tests

--- src/transactions/transactions.service.spec.ts
+++ src/transactions/transactions.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionsService } from './transactions.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { EntityManager } from 'typeorm';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionsService],
+      providers: [
+        TransactionsService,
+        {
+          provide: getRepositoryToken(Transaction),
+          useValue: {},
+        },
+        {
+          provide: EntityManager,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<TransactionsService>(TransactionsService);
